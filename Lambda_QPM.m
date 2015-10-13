65db63a21f1959c306612ffa35ce75da13b61753
Amar Kumar <amarkumariitdelhi@gmail.com>
2015-10-13 15:21:25 +0530

    Update Lambda_QPM.m

--- Lambda_QPM.m
+++ Lambda_QPM.m
@@ -8,6 +8,12 @@ idler = solve (1/pump == 1/signal + 1/idlr , idlr); %Conservation of Energy
 
 [n_io, alpha_ioy, alpha_ioz] = give_stats(idler,w, h, T,'o');
 
-Lambda = solve( 1/Lambda_0 == n_po/pump + n_se/signal +n_io/idler ,Lambda_0 );
+% Lambda = solve( 1/Lambda_0 == n_po/pump - n_se/signal -n_io/idler ,Lambda_0 );
+n_po
+n_se
+n_io = double(n_io)
+main_signal = signal
+main_idler = double(idler)
+Lambda = double((main_signal *pump)/ (n_po*main_signal - n_se*pump - n_io*main_signal + n_io*pump));
 end
 %Result in nm
